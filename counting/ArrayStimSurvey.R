1b239047b206167ae3e8bee23aaad6fbd1aecfb8
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2009-09-30 04:13:07 +0000

    included array counting
    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/allarrays/trunk@8234 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ counting/ArrayStimSurvey.R
@@ -0,0 +1,202 @@
+library(rjson)
+library(httpRequest)
+library(chron)
+
+util.dir <- file.path(Sys.getenv("AA"),"utils")
+source(file.path(Sys.getenv("HOME"),"bin/R/MatrixPrintFormat.R"))
+source(paste(util.dir,"httpget.R",sep="/"))
+source(paste(util.dir,"utilitiesMetaData.R",sep="/"))
+source(paste(util.dir,"utilitiesSampleGroup.R",sep="/"))
+
+##the period from March 1, 2009 - August 30, 2009 (Not sure about this!)
+## format m/d/y
+startDate <- "3/1/09"
+endDate <- "8/30/09"
+
+term.array="/sampleData/microarray/chips/Mouse Exon"
+
+##term.array="/sampleData/microarray/chips/Mouse 430 2.0"
+
+##term.array="/sampleData/microarray/chips/Isbimm"
+
+##term.array="/sampleData/microarray/chips/Mouse Promoter 1.0R"
+
+### First version: Agnostic to Project
+
+### Get all arrays ?
+
+### Find all unique combinations of
+### Cell Type
+
+### Strain
+
+### Ignore (in this round) Sex, Stim 2, 
+
+## 1. Find all unique combinations
+
+##term.ct="Dendritic Cell"
+##term.ct="BMDM"
+##names(term.ct)="Cell Type"
+##term.str="Bl6"
+##names(term.str)="Strain"
+##term.stim="R848"
+##names(term.stim)="Stimulus 1"
+##term.t="\"0120\""
+##names(term.t)="Time 1" 
+
+
+names(term.array) = "chip"
+termlist <- list()
+termlist[[1]] <- term.array
+
+obj.1 <- searchByNameValue(termListHttpForm(termlist))
+##length(obj.1) is 538 on 5-14-09 for exon arrays
+##length(obj.1) is 600 on 7-24-09 for exon arrays
+##length(obj.1) is 648 on 8-30-09 for exon arrays
+##length(obj.1) is 672 on 9-28-09 for exon arrays
+
+## Filter by time if desired
+obj <- filterArrayListByDate(obj.1,startDate,endDate)
+
+cat(strsplit(term.array,split="/")[[1]][5],"\n")
+cat("Start date:",startDate,"End date:",endDate,"\n")
+cat("Before time range filtering:",length(obj.1),"\n")
+cat("After time range filtering:",length(obj),"\n")
+
+all.cell.types <- unique(unlist(lapply(obj,"[[","Cell Type")))
+all.times <- sort(unique(unlist(lapply(obj,"[[","Time 1"))))
+all.times <- all.times[sort(as.numeric(all.times),index.return=TRUE)$ix]
+## last one as the string sort can fail
+
+## Cell type for all samples
+## 60 samples with no cell type
+all.cell.type <- as.character(lapply(obj,"[[","Cell Type"))
+cell.type.null <- which(as.character(lapply(obj,"[[","Cell Type"))=="NULL")
+unlist(lapply(obj[cell.type.null],"[[","name"))
+## Lack ofo metadata, including Investigator
+## Seems Thunder data is here!
+## The others might be VL's titration experiments
+
+## Time 1 for all samples
+## 84 experiments have no Time 1 
+all.time1 <- as.character(lapply(obj,"[[","Time 1"))
+time1.null <- which(as.character(lapply(obj,"[[","Time 1"))=="NULL")
+
+## Stime 1 for all samples 
+all.stim1 <- as.character(lapply(obj,"[[","Stimulus 1"))
+
+baddies <- which(all.cell.type =="NULL" | all.stim1 == "NULL" | all.cell.type == "NULL" )
+## 84 are missing one of the above attributes
+## In fact, these 84 are all missing stim1 and time1
+
+## 24 with cell type but not Time1, Stim1
+inds.24 <- which(all.cell.type!="NULL" & all.stim1=="NULL")
+## These all seem like "unstim", for a variety of strains
+## They do not have "Stim 1==Unstim" 
+
+## 451 have Strain,Stimulus 1, Time 1
+
+
+ 
+## 410, excluding double stims
+## 451, with double stims (41 double stim *samples* ) 
+
+inds.dbls <- which(as.character(lapply(obj,"[[","Stimulus 2")) !=  "NULL" )
+ 
+doobles <- cbind(
+                 unlist(lapply(obj[inds.dbls],"[[","Cell Type")),
+                 unlist(lapply(obj[inds.dbls],"[[","Strain")),
+                 unlist(lapply(obj[inds.dbls],"[[","Stimulus 1")),
+                 unlist(lapply(obj[inds.dbls],"[[","Time 1")),
+                 unlist(lapply(obj[inds.dbls],"[[","Stimulus 2")),
+                 unlist(lapply(obj[inds.dbls],"[[","Time 2"))
+                 )
+## Various ways to organize these
+## Need to account for Stim1/Stim2 labeling arbitrarity
+## In most cases, time1=time2 (sampling time)
+## In some case time != time2 (administration time )
+
+
+## Or April 2009, three prime arrys
+##all.cell.types <- "BMDM"
+##all.times <- c("0000","0020","0040","0060","0080","0100","0120","0180","0240","0300","0360","0420","0480","0720","1080","1440","2880")
+
+## Or, May 2009, exon arrays
+##all.cell.types <- c("BMDM","Dendritic Cell","Lung","MycBMM","mTEC","Splenic Dendritic Cell","Lung Dendritic Cell")
+##all.times <- c("0000","0060","0120","0180","0240","0360","0480","0540","0720","1020","1080","1440")
+
+metadataMat <- character()
+timetallyMat <- numeric()
+
+for ( cell.type in all.cell.types ){
+
+  term.ct <-  cell.type
+  names(term.ct) <- "Cell Type"
+  termlist[[2]] <- term.ct
+
+  objj <- searchByNameValue(termListHttpForm(termlist))
+
+  ## Filter by time if desired
+  objj <- filterArrayListByDate(objj,startDate,endDate)  
+  
+  ## Ditch samples with Stim2 for now
+  ##objj <- removeStim2(objj) 
+  ## For clean indexing operations below, remove the nulls
+  time1.null <- which(as.character(lapply(objj,"[[","Time 1"))=="NULL")
+  if ( length(time1.null) > 0){
+    objj <- objj[-time1.null]
+  }
+  stim1.null <- which(as.character(lapply(objj,"[[","Stimulus 1"))=="NULL")
+  if ( length(stim1.null) > 0){
+    objj <- objj[-stim1.null]
+  }
+  strain.null <- which(as.character(lapply(objj,"[[","Strain"))=="NULL")
+  if ( length(strain.null) > 0){
+    objj <- objj[-strain.null]
+  }
+    
+  stim1s <- unique(unlist(lapply(objj,"[[","Stimulus 1")))
+  
+  for ( stim1 in stim1s ){
+    inds <- which(unlist(lapply(objj,"[[","Stimulus 1"))==stim1)
+    objjj <- objj[inds] ## now has unique cell type and stim
+    strains <- unique(unlist(lapply(objjj,"[[","Strain")))
+    for ( strain in strains ){
+      inds <- which(unlist(lapply(objjj,"[[","Strain"))==strain)
+      timetally.1 <- table(unlist(lapply(objjj[inds],"[[","Time 1")))
+      timetally.2 <- as.numeric(timetally.1)
+      names(timetally.2) <- names(timetally.1)
+      timetally.3 <- vector(length=length(all.times))
+      names(timetally.3) <- all.times
+      timetally.3[names(timetally.2)] <- as.numeric(timetally.2)
+      if ( !is.null(ncol(timetallyMat))){
+        if ( ncol(timetallyMat) != length(timetally.3) ){
+          stop("Mismatch\n")
+        }
+      }
+      timetallyMat <- rbind(timetallyMat,timetally.3)
+      metadataMat <- rbind(metadataMat,c(cell.type,stim1,strain))
+    }
+  }
+}
+rownames(timetallyMat) <- NULL
+ 
+tt2 <- cbind(timetallyMat,apply(timetallyMat,1,sum))
+tt3 <- rbind(tt2,apply(tt2,2,sum))
+
+mm3 <- rbind(metadataMat,c("Column Total","",""))
+
+outMat <- cbind(mm3,tt3)
+outMat <- rbind(c("Cell Type","Stimulus 1","Strain",all.times,"Row Total"),outMat)
+rownames(outMat) <- NULL
+colnames(outMat) <- NULL
+write.table(outMat,file="AllMetaData.tsv",sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+
+cat("Have Cell Type,Strain, Stimulus 1:",sum(timetallyMat),"\n")
+
+outMat <- rbind(c("Cell Type","Strain","Stimulus 1","Time 1","Stimulus 2","Time 2"),doobles)
+write.table(outMat,file="Doubles.tsv",sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+
+##write.table(outMat,file="AllThreePrimeMetaData.tsv",sep="\t",quote=FALSE)
+
+
---
+++ counting/glueCounter.R
@@ -0,0 +1,88 @@
+
+library(rjson)
+library(httpRequest)
+library(chron)
+
+util.dir <- file.path(Sys.getenv("AA"),"utils")
+source(file.path(Sys.getenv("HOME"),"bin/R/MatrixPrintFormat.R"))
+source(paste(util.dir,"httpget.R",sep="/"))
+source(paste(util.dir,"utilitiesMetaData.R",sep="/"))
+
+path = "/sampleData/microarray/samples/Aderem%20Lab"
+
+##This report covers
+##the period from August 30, 2008 - February 28, 2009.
+## format m/d/y
+startDate <- "8/30/08"
+endDate <- "2/28/09"
+
+
+##the period from March 1, 2008 - August 29, 2009 (Not sure about this!)
+## format m/d/y
+startDate <- "3/1/09"
+endDate <- "8/29/09"
+
+chips <- c("Mouse Exon","Mouse 430 2.0","Mouse Promoter 1.0R","Isbimm","Human U133 Plus 2.0","Human U133A","Human Exon","Rhesus Macaque")
+
+glueprojects <- c(
+##"Aderem Custom ChIP",
+"TLR Specificity",
+"Litvak Samples",
+"Exon Arrays",
+"LPS Time Course",
+"PIC Time Course",
+##"Aderem Promoter Arrays",
+"Influenza",
+"Liz Gold Arrays",
+"Dendritic Cell Arrays",
+"Leishmania Infected Cells",
+"Viral Infections",
+"Glue Genomics",
+"GLUE grant"  )
+
+collect <- numeric()
+keepers <- character()
+
+for ( proj in glueprojects ){
+##for ( i in 1:2 ){
+
+  cat(proj,"\n")
+  proj.p20 <- whiteSpaceURLform(proj)
+  proj.path <- paste(path,proj.p20,sep="/")
+  response = getToHost("innate-immunity.systemsbiology.net",paste("/addama-rest/primary-repo?PATH=",proj.path,sep=""),"",port=80)
+  response = sub("1ff8","",response)
+  response = sub("\r\n1f29\r\n","",response)
+  response = gsub("\r\n1ff8\r\n","",response)
+  response = gsub("\r\n93\r\n","",response)
+  response = gsub("\r\n[[:alnum:]]{1,50}\r\n","",response)
+  ## chop HTTP header stuff in response
+  resp = substr(response,regexpr("\\{",response)[1],nchar(response))
+  resp = sub("}\r.*","}",resp)
+  object = fromJSON(resp)
+
+  listi <- object$children
+
+  if ( length(listi ) >= 1){
+
+    oo <- table(arrayStats(listi,startDate,endDate))
+
+    if ( length(oo)>=1 & sum(oo)>0 ) {
+    
+      blank <- numeric(length=length(chips))
+      names(blank) <- chips
+      blank[names(oo)] <- as.numeric(oo)   
+      collect <- rbind(collect,blank)
+      keepers <- c(keepers,proj)
+    }
+  }
+  
+  ##samplecounts[proj] <- length(object[["children"]])
+  
+}
+
+rownames(collect) <- keepers
+
+write.table(matrixPrintFormat(collect),file="1MarchTo26Aug2009.tsv",sep='\t',quote=FALSE,row.names=FALSE,col.names=FALSE)
+
+
+
