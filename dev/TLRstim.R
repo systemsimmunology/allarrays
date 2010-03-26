aac8b8ede9b0f1d5f5f17075cfa98c0ee6f407fe
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-03-26 23:09:51 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/allarrays/trunk@9774 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- dev/TLRstim.R
+++ dev/TLRstim.R
@@ -5,4 +5,4 @@
 dm.lps.exon <- dm.exon[,CSSs.tc.exon[["BMDM_Bl6_LPS__Female"]][["DM Column"]]]
 dm.lps.3prime <- dm.3prime[,CSSs.tc.3prime[["BMDM_Bl6_LPS__Female"]][["DM Column"]]]
 
-save(file="lps.RData",list=c("dm.lps.exon","dm.lps.3prime"))
+save(file="~/chipseq/data/lps.RData",list=c("dm.lps.exon","dm.lps.3prime","CSSs.tc.exon","CSSs.tc.3prime"))
