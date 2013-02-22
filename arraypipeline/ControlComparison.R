3933db9bb5eaaacc2fa989ca32a6186e900264f0
Vesteinn Thorsson <thorsson@systemsbiology.org>
2013-02-21 19:49:17 -0800

    ComparePlatforms.Rmd and lambda.cutoff estimator

--- arraypipeline/ControlComparison.R
+++ arraypipeline/ControlComparison.R
@@ -3,7 +3,7 @@
 ##grep 0000 $AA/auxfiles/ThreePrimeMasterFile.tsv | awk '{OFS="\t"; print $3,$2}' > tempfile
 ##Edit to yield only triplicated 
 ##mv tempfile unstim-groupings.tsv
-##../VERASAMpipeline.sh $AA/data/20100426.curated.3prime/emat.tsv unstim-groupings.tsv BMDM_Bl6_LPS_0000___Female
+##../VERASAMpipeline.sh $AA/data/YYYYMMDD.curated.3prime/emat.tsv unstim-groupings.tsv BMDM_Bl6_LPS_0000___Female
 
 ls <- read.matrix("~/allarrays/arraypipeline/tmp/expression.lambdas")
 
@@ -12,5 +12,13 @@ for ( n in 1:ncol(ls)) {
   collect <- c(collect, quantile(ls[,n],0.95))
 }
 
-mean(collect) ## 26.61275 for 0.95;  66.31579 for 0.99
+mean(collect) 
+
+
+collect <- numeric()
+for ( n in 1:ncol(ls)) {
+  collect <- c(collect, quantile(ls[,n],0.99))
+}
+
+mean(collect) 
 
